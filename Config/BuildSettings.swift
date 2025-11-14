76870d35b0a2f5f83451652a7a8679eeeda7a302
zhgres <zhgres@gmail.com>
2025-11-14 12:58:38 +0500

    Use zhgres push gateway

--- Config/BuildSettings.swift
+++ Config/BuildSettings.swift
@@ -102,7 +102,7 @@ final class BuildSettings: NSObject {
     static let serverConfigDefaultIdentityServerUrlString = "https://vector.im"
         
     static var serverConfigSygnalAPIUrlString: String {
-        MDMSettings.serverConfigSygnalAPIUrlString ?? "https://matrix.org/_matrix/push/v1/notify"
+        MDMSettings.serverConfigSygnalAPIUrlString ?? "https://push.zhgres.kz/_matrix/push/v1/notify"
     }
     
     // MARK: - Legal URLs
